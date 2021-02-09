53aef37af3dc9666f0009ed873cda6f8de763579
douglastofoli <tofoli.douglas@hotmail.com>
2021-02-09 11:35:48 -0300

    added then catch on createConnection

--- src/database/connections.ts
+++ src/database/connections.ts
@@ -4,5 +4,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-createConnection(String(process.env.DB1_NAME));
-createConnection(String(process.env.DB2_NAME));
+createConnection(String(process.env.DB1_NAME)) // conecta na base oracle
+  .then(() => {
+    console.log('Database OracleDB connected with success!');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+createConnection(String(process.env.DB2_NAME)) // conecta na base postgres
+  .then(() => {
+    console.log('Database PostgreSQL connected with success!');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
