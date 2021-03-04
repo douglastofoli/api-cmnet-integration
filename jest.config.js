fa87af9ffaff06fda98d47b35bac96432c2b3929
douglastofoli <tofoli.douglas@hotmail.com>
2021-03-04 16:23:08 -0300

    fix bug jest eslint

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['node_modules', 'dist'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1'
   }
