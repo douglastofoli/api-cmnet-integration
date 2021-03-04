5d482357113b38276c993891dda82730aae9403a
douglastofoli <tofoli.douglas@hotmail.com>
2021-03-04 19:00:27 -0300

    test

--- src/configs/jwtConfig.ts
+++ src/configs/jwtConfig.ts
@@ -1,7 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 export default {
   jwtSecret: String(process.env.JWT_SECRET)
 };
--- src/database/connections.ts
+++ src/database/connections.ts
@@ -1,8 +1,5 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 createConnection(String(process.env.DB1_NAME)) // conecta na base oracle
   .then(() => {
