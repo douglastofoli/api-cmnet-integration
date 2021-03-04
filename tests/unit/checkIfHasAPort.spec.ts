012ac68edd4acc47a5347cece086457cff7bedb2
douglastofoli <tofoli.douglas@hotmail.com>
2021-03-04 18:44:39 -0300

    test

--- tests/unit/checkIfHasAPort.spec.ts
+++ tests/unit/checkIfHasAPort.spec.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log(process.env.HTTP_PORT)
+
 describe('HTTP_PORT', () => {
   it('should have 4 numbers', () => {
     expect(process.env.HTTP_PORT).toHaveLength(4); // tem que ter 32 caracteres
