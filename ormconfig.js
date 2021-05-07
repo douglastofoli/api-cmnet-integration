cc5722134263358cdb2b6bd118e26d83a51ad8f4
douglastofoli <tofoli.douglas@hotmail.com>
2021-05-07 11:59:56 +0000

    connections config

--- ormconfig.js
+++ ormconfig.js
@@ -7,7 +7,7 @@ module.exports = [
     name: process.env.DB1_NAME,
     type: process.env.DB1_TYPE,
     host: process.env.DB1_HOST,
-    port: process.env.DB1_PORT,
+    port: Number(process.env.DB1_PORT),
     username: process.env.DB1_USERNAME,
     password: process.env.DB1_PASSWORD,
     database: process.env.DB1_DATABASE,
@@ -24,7 +24,7 @@ module.exports = [
     name: process.env.DB2_NAME,
     type: process.env.DB2_TYPE,
     host: process.env.DB2_HOST,
-    port: process.env.DB2_PORT,
+    port: Number(process.env.DB2_PORT),
     username: process.env.DB2_USERNAME,
     password: process.env.DB2_PASSWORD,
     database: process.env.DB2_DATABASE,
@@ -42,7 +42,7 @@ module.exports = [
     name: process.env.DB3_NAME,
     type: process.env.DB3_TYPE,
     host: process.env.DB3_HOST,
-    port: process.env.DB3_PORT,
+    port: Number(process.env.DB3_PORT),
     username: process.env.DB3_USERNAME,
     password: process.env.DB3_PASSWORD,
     database: process.env.DB3_DATABASE,
@@ -54,6 +54,23 @@ module.exports = [
     cli: {
       entitiesDir: 'src/entities/esolution',
       migrationsDir: 'src/database/migrations/esolution'
+    }
+  },
+  {
+    name: process.env.DB4_NAME,
+    type: process.env.DB4_TYPE,
+    host: process.env.DB4_HOST,
+    port: Number(process.env.DB4_PORT),
+    username: process.env.DB4_USERNAME,
+    password: process.env.DB4_PASSWORD,
+    database: process.env.DB4_DATABASE,
+    synchronize: process.env.DB4_SYNCHRONIZE,
+    logging: process.env.DB4_LOGGING,
+    entities: ['src/entities/mssql_esolution/**/*.ts'],
+    migrations: ['src/database/migrations/mssql_esolution/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/entities/mssql_esolution',
+      migrationsDir: 'src/database/migrations/mssql_esolution'
     }
   }
 ];
